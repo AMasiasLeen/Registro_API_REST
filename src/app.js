7d40b03df45b209db5cc1155958cb66d549152ac
AMasiasLeen <leenmasias@gmail.com>
2024-07-17 22:06:39 -0500

    Update.
    - Se instalaron la dependencias para el uso de JWT
    - Se creo el respectivo controlador, migración, modelo y ruta para la implementación del JWT
    - Se añadieron vistas para realizar pequeñas comprobaciones

--- src/app.js
+++ src/app.js
@@ -3,17 +3,47 @@ require("dotenv").config()
 const express = require("express")
 const port = process.env.APP_PORT
 const app = express()
-app.use(express.json())
+const jwt = require('jsonwebtoken');
+
+const path = require("path")
+
+app.use(express.json());
+
 
 const routes = require('./routes');
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views","index.html"))
+})
+
+
+// Middleware para autenticar usuarios
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
 
 app.use("/Machinery", routes.MachineryRoutes)
 app.use("/Register", routes.RegisterRoutes)
 app.use("/Owner", routes.OwnerRoutes)
 app.use("/Operator", routes.OperatorRoutes)
-
-
+app.use("OwnerUser",routes.UserRoutes)
 
 app.listen(port, () => {
-    console.log("Uh oh")
+    console.log("Server Online!")
 })
+
+
+
+
